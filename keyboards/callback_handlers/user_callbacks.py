802b8024548ac9c7789631095b2e50e51be59ddc
losimen <koval.pavlo03@gmail.com>
2023-05-14 11:46:55 +0300

    hello world

--- keyboards/callback_handlers/user_callbacks.py
+++ keyboards/callback_handlers/user_callbacks.py
@@ -3,8 +3,6 @@ from aiogram.dispatcher.filters import Text
 from menus import user_menus
 from aiogram import types
 
-
-
 @dp.callback_query_handler(text = 'void')
 async def void_call(callback: types.CallbackQuery):
     print('hello')
--- menus/user_menus.py
+++ menus/user_menus.py
@@ -3,5 +3,4 @@ import keyboards.keyboards_generator.user_keyboards as keyboard_generator
 
 
 async def welcome_user(message: types.Message):
-    await message.answer(text='🇺🇦 Оберіть мову\n\n'
-                              '🇷🇺 Выберите язык\n\n', reply_markup=keyboard_generator.welcome_user())
+    await message.answer(text='Hello world', reply_markup=keyboard_generator.welcome_user())
