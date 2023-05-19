e8ef1c93a5973b912726f98a96ebfc3163b9810a
losimen <koval.pavlo03@gmail.com>
2023-05-19 13:21:13 +0300

    readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Creditor bot
+## Description
+Bot for managing your income and expenses. It can be used for personal accounting.
+## Installation
+1. Clone this repository
+2. Install requirements
+3. Create a file `config.py` in the root directory of the project
+`config.py` example:
+````python
+ADMIN_ID = 
+BOT_TOKEN = 
+
+
+TABLE_NAME = 
+
+MONGO_USERNAME = 
+MONGO_PASS = 
+MONGO_CLUSTER = 
+````
+
+## Usage
+- `/start` - start the bot or main menu
+- `/expense` - add expense
+- `/income` - add income
--- handlers/user_commands.py
+++ handlers/user_commands.py
@@ -1,7 +1,12 @@
 from aiogram import Dispatcher
 from menus import user_menus
+from FSM import income_fsm, expense_fsm
 
 
 def register_messages_client(dp: Dispatcher):
     dp.register_message_handler(user_menus.main_menu_menu,
                                 commands=['start'])
+    dp.register_message_handler(income_fsm.fsm_start_income_form,
+                                commands=['income'])
+    dp.register_message_handler(expense_fsm.fsm_start_expense_form,
+                                commands=['expense'])
